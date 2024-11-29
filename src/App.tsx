f147461d8bd718864f4c9a5b6baef07942b4ae74
Muhammad Ibrahim Umar <muhammadbmw69@gmail.com>
2024-11-29 17:18:30 +0100

    feat: setup custom fonts and theme styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-6xl font-extrabold">
+      <h1 className="font-zentry text-6xl text-violet-300 ">
         Are you ready for the conversation ?
       </h1>
+      <h1 className="font-general text-6xl ">
+        Are you ready for the conversation ?
+      </h1>
+      <h1 className="font-robert-medium text-6xl ">
+        Are you ready for the conversation ?
+      </h1>
+      <h1 className="font-robert-regular text-6xl ">
+        Are you ready for the conversation ?
+      </h1>
+      <h1 className="font-circular text-6xl ">
+        Are you ready for the conversation ?
+      </h1>
+
+      {/* Fonts Integration Checklist
+        - Zentry  - OK
+        - General - OK
+        - Robert Medium - OK
+        - Robert Regular - OK
+        - Circular - OK
+      */}
     </main>
   );
 };
