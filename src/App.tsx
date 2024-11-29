0a646caff1538aa86fbc56f117e8a55710fb450e
Muhammad Ibrahim Umar <muhammadbmw69@gmail.com>
2024-11-29 15:00:23 +0100

    intial project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-6xl font-extrabold">
+        Are you ready for the conversation ?
+      </h1>
+    </main>
+  );
+};
+
+export default App;
