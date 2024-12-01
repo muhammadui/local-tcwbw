1f27f496824b6c9c38563cc7b3c1c6c32d467f7d
Muhammad Ibrahim Umar <muhammadbmw69@gmail.com>
2024-12-01 03:54:56 +0100

    feat: finished hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
+import Hero from "./components/Hero";
+
 const App = () => {
   return (
-    <main>
-      <h1 className="font-zentry text-6xl text-violet-300 ">
-        Are you ready for the conversation ?
-      </h1>
-      <h1 className="font-general text-6xl ">
-        Are you ready for the conversation ?
-      </h1>
-      <h1 className="font-robert-medium text-6xl ">
-        Are you ready for the conversation ?
-      </h1>
-      <h1 className="font-robert-regular text-6xl ">
-        Are you ready for the conversation ?
-      </h1>
-      <h1 className="font-circular text-6xl ">
-        Are you ready for the conversation ?
-      </h1>
-
-      {/* Fonts Integration Checklist
-        - Zentry  - OK
-        - General - OK
-        - Robert Medium - OK
-        - Robert Regular - OK
-        - Circular - OK
-      */}
+    <main className="relative min-h-screen overflow-x-hidden">
+      <Hero />
+      <section className="z-0 min-h-screen bg-blue-500" />
     </main>
   );
 };
