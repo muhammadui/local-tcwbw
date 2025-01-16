8c2bfb399a536dc8a641a907b5f6b029224db2a7
Muhammad Ibrahim Umar <muhammadbmw69@gmail.com>
2025-01-16 08:23:34 +0100

    feat: added new features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -121,10 +121,10 @@ const Features = () => (
   <section className="bg-black pb-52">
     <div className="container mx-auto px-3 md:px-10">
       <div className="px-5 py-32">
-        <p className="font-circular-web text-lg text-blue-50">
+        <p className="font-circular text-lg text-blue-50">
           Into the Metagame Layer
         </p>
-        <p className="font-circular-web max-w-md text-lg text-blue-50 opacity-50">
+        <p className="max-w-md font-circular text-lg text-blue-50 opacity-50">
           Immerse yourself in a rich and ever-expanding universe where a vibrant
           array of products converge into an interconnected overlay experience
           on your world.
